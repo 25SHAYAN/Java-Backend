79e05b3505ee0a65f568be530fe36b25135f1483
Nagashayan K <136794795+25SHAYAN@users.noreply.github.com>
2026-02-02 11:13:21 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,34 +1,16 @@
-package OOPs.P3;
- class Car {
-    String brand;
-    String color;
-   private int speed;
+package OOPs.P4;
+
+import java.sql.SQLOutput;
+
+record Bike(String bike,int speed){}
 
 
-    public void drive() {
-        System.out.println(brand + " is driving at " + speed);
-    }
-    //getter and setter
-
-     public void setSpeed(int speed) {
-         this.speed = speed;
-     }
-
-     public int getSpeed() {
-        if(speed<0)
-        {
-            return 0;
-        }
-         return speed;
-     }
-
- }
 public class Main {
     public static void main(String[] args) {
-Car c1 = new Car();
-c1.setSpeed(-120);
-        System.out.println(c1.getSpeed());
-        c1.setSpeed(150);
-        System.out.println(c1.getSpeed());
+        //RECORDS
+        Bike bike = new Bike("BMW",222);
+        System.out.println("Speed  " + bike.speed());
+        System.out.println("Brand " + bike.bike());
+        System.out.println(bike); //tostring
     }
 }
